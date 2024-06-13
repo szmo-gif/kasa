060a63eecbaef24c14e49d9905d5317f74d1e11a
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-06-13 12:07:08 +0200

    rajout dossier image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
+    <>
+      <h1>Accueil</h1>
       <nav>
         <Link to="/usingSheet">Fiche logement</Link>
       </nav>
+    </>
   )
 }
